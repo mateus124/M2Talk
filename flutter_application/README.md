e66803a7fd9153334ee443866ff5f288896ceaa0
mateus124 <alvesmateusq00@gmail.com>
2026-06-07 18:22:12 -0300

    feat: initial mobile client

---
+++ flutter_application/README.md
@@ -0,0 +1,3 @@
+# M2Talk Mobile
+
+Aplicativo Flutter para Android do M2Talk.
---
+++ flutter_application/android/app/src/main/kotlin/com/m2talk/mobile/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.m2talk.mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
